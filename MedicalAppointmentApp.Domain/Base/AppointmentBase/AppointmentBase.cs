405832dda0b1e8f52823af8874a300327df655f6
Derik-Infante <Derik-Infante>
2024-09-25 05:46:16 -0400

    Entitades AppointmentAppointments, AppointmentsDoctor, Insu

---
+++ MedicalAppointmentApp.Domain/Base/AppointmentBase/AppointmentBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalAppointmentApp.Domain.Base.AppointmentBase
+{
+    public class AppointmentBase
+    {
+        int DoctorID { get; set; }
+    }
+}
---
+++ MedicalAppointmentApp.Domain/Entities/AppointmentsDoctorAvailability/AppointmentsDoctorAvailability.cs
@@ -0,0 +1,17 @@
+﻿using MedicalAppointmentApp.Domain.Base.AppointmentBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalAppointmentApp.Domain.Entities.AppointmentsDoctorAvailability
+{
+    public sealed class AppointmentsDoctorAvailability : AppointmentBase
+    {
+        int AvailabilityID { get; set; }   
+        DateTime AvailableDate {  get; set; }
+        TimeOnly  StartTime { get; set; }
+        TimeOnly EndTime { get; set; }
+    }
+}
