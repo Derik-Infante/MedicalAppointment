108c980c422c0b57d9441869601e7bc3b647b84b
Ysauri Jiménez <ysmajimo@gmail.com>
2024-10-04 22:46:40 -0400

    error en mi rama resuelto

---
+++ MedicalAppointmentApp.Domain/Entities/System/systemStatus.cs
@@ -0,0 +1,14 @@
+﻿using MedicalAppointmentApp.Domain.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalAppointmentApp.Domain.Entities.System
+{
+    public class systemStatus:InfoBase
+    {
+        int statusID { get; set; }
+    }
+}
