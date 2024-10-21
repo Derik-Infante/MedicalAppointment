b8c9c90987ede21bc2b5765d5e9db9f79f6ade7a
Ysauri Jiménez <ysmajimo@gmail.com>
2024-10-20 20:04:10 -0400

    ya ahora sí está lista la capa de dominio

--- MedicalAppointmentApp.Domain/Entities/System/Notifications.cs
+++ MedicalAppointmentApp.Domain/Entities/System/Notifications.cs
@@ -1,8 +1,13 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace MedicalAppointmentApp.Domain.Entities.System
 {
+    [Table("notifications", Schema = "sistema")]
     public class Notifications
     {
+        [Key]
         public int NotificationID { get; set; }
 
         public int UserID { get; set; }
--- MedicalAppointmentApp.Domain/Entities/System/Roles.cs
+++ MedicalAppointmentApp.Domain/Entities/System/Roles.cs
@@ -1,9 +1,13 @@
 ﻿using MedicalAppointmentApp.Domain.Base;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MedicalAppointmentApp.Domain.Entities.System
 {
+    [Table("roles", Schema = "sistema")]
     public class Roles: BaseEntity
-    { 
+    {
+        [Key]
         public int RoleID { get; set; }
 
         public string? RoleName { get; set; }
