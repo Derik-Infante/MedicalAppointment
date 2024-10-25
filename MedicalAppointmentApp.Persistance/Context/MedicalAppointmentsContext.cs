64d708abe1c99635e9967736324fdac6ee7a947d
Ysauri Jiménez <ysmajimo@gmail.com>
2024-10-25 16:53:03 -0400

    working in persistence

---
+++ MedicalAppointmentApp.Persistance/Context/MedicalAppointmentsContext.cs
@@ -0,0 +1,21 @@
+﻿using MedicalAppointmentApp.Domain.Entities.AppointmentsAppointments;
+using MedicalAppointmentApp.Domain.Entities.System;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace MedicalAppointmentApp.Persistance.Context
+{
+    public partial class MedicalAppointmentsContext : DbContext
+    {
+        public MedicalAppointmentsContext(DbContextOptions<MedicalAppointmentsContext> options) : base(options)
+        {
+
+        }
+
+        #region "System Entities"
+        public DbSet<Notifications> Notifications { get; set; }
+        public DbSet<Roles> Roles { get; set; }
+        public DbSet<Status> Status { get; set; }
+        #endregion
+    }
+}
