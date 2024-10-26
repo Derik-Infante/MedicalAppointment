cdcbf3e4542607a2ba820fbccb09aa5aaa8f5847
Ysauri Jiménez <ysmajimo@gmail.com>
2024-10-25 23:54:10 -0400

    casi casi terminado p

---
+++ MedicalAppointmentApp.Persistance/Models/Notification.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MedicalAppointmentApp.Persistance.Models;
+
+public partial class Notification
+{
+    public int NotificationId { get; set; }
+
+    public int UserId { get; set; }
+
+    public string Message { get; set; } = null!;
+
+    public DateTime? SentAt { get; set; }
+}
