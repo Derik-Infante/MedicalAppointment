69fdab42ca457e0bbe67f691287c4bf9ca999abd
Ysauri Jiménez <ysmajimo@gmail.com>
2024-10-25 21:19:45 -0400

    casi terminado persistencia

---
+++ MedicalAppointmentApp.Persistance/Interfaces/System/INotificationsRepository.cs
@@ -0,0 +1,11 @@
+﻿
+using MedicalAppointmentApp.Domain.Entities.System;
+using MedicalAppointmentApp.Domain.Repositories;
+
+namespace MedicalAppointmentApp.Persistance.Interfaces.System
+{
+    public interface INotificationsRepository : IBaseRepository<Notifications> 
+    {
+
+    }
+}
---
+++ MedicalAppointmentApp.Persistance/Repositories/NotificationsRepository.cs
@@ -0,0 +1,23 @@
+﻿
+
+using MedicalAppointmentApp.Domain.Entities.System;
+using MedicalAppointmentApp.Domain.Result;
+using MedicalAppointmentApp.Persistance.Base;
+using MedicalAppointmentApp.Persistance.Context;
+using MedicalAppointmentApp.Persistance.Interfaces.System;
+
+namespace MedicalAppointmentApp.Persistance.Repositories
+{
+    public class NotificationsRepository : BaseRepository<Notifications>, INotificationsRepository
+    {
+        public NotificationsRepository(MedicalAppointmentsContext medicalAppointmentsContext): base(medicalAppointmentsContext)
+        {
+        }
+
+        public override Task<OperationResult> Update(Notifications entity)
+        {
+            return base.Update(entity);
+        }
+
+    }
+}
