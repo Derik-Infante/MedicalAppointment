706df2a7ed3608b2f8169cd0f15d72bdd9405144
Ysauri Jiménez <ysmajimo@gmail.com>
2024-12-05 00:35:20 -0400

    wk

---
+++ MedicalAppointmentApp.Test/Context/MedicalAppointmentsMockContext.cs
@@ -0,0 +1,17 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using MedicalAppointmentApp.Persistance.Context;
+
+namespace MedicalAppointmentApp.Test.Context
+{
+    public class MedicalAppointmentsMockContext : MedicalAppointmentsContext
+    {
+        public MedicalAppointmentsMockContext(DbContextOptions<MedicalAppointmentsContext> options): base(options)
+        {}
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("bolectodb");
+        }
+    }
+}
